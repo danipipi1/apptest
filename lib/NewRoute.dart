5cdd3ffb170c61778bcfbd6aadd886520402c3c2
惮晰 <492273770@qq.xom>
2019-05-16 19:17:40 +0800

    Initial commit

---
+++ lib/NewRoute.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class NewRoute extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    var args=ModalRoute.of(context).settings.arguments;
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("New route"),
+      ),
+      body: Center(
+        child: Text(args??="11111111111111"),
+      ),
+    );
+  }
+}
